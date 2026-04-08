6a460ac7f5b42e6a0a600e3c9f2a97b5f48f9dd6
frontmqaj91 <qahtanm9@gmail.com>
2026-04-08 09:01:46 -0400

    fix index.js as serverless function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+  const file = path.join(process.cwd(), 'public', 'index.html');
+  const html = fs.readFileSync(file, 'utf8');
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(html);
+};
